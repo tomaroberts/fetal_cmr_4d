6ab587137c9e523676a33d7a2ef7b5d6d7fcd412
Tom Roberts <t.roberts@kcl.ac.uk>
2021-03-05 17:20:20 +0000

    wip: k-t sweep cardsync 4d model recon

--- wip/sweep/cardsync_sweep_cine_vol.m
+++ wip/sweep/cardsync_sweep_cine_vol.m
@@ -16,7 +16,7 @@ function S = cardsync_sweep_cine_vol( S, varargin )
 
 default.reconDir        = pwd;
 default.resultsDir      = pwd;
-default.ktblocksize     = 32;
+default.ktBlockSize     = 32;
 default.isVerbose       = false;
 
 
@@ -40,7 +40,7 @@ add_param_fn(   p, 'recondir', default.reconDir, ...
 add_param_fn(   p, 'resultsdir', default.resultsDir, ...
     @(x) validateattributes( x, {'char'}, {'vector'}, mfilename) );
 
-add_param_fn(  p, 'ktblocksize', default.ktBlockSize, ...
+add_param_fn(  p, 'ktBlockSize', default.ktBlockSize, ...
         @(x) validateattributes( x, {'numeric'}, {'scalar','nonnegative'}, mfilename ) );
 
 add_param_fn(   p, 'verbose', default.isVerbose, ...
@@ -50,7 +50,7 @@ parse( p, S, varargin{:} );
 
 reconDir        = p.Results.recondir;
 resultsDir      = p.Results.resultsdir;
-ktBlockSize     = p.Results.ktblocksize;
+ktBlockSize     = p.Results.ktBlockSize;
 isVerbose       = p.Results.verbose;
 
 
@@ -60,12 +60,15 @@ dataDir     = fullfile( reconDir, 'data' );
 maskDir     = fullfile( reconDir, 'mask' );
 ktreconDir  = fullfile( reconDir, 'ktrecon' );
 cineVolDir  = fullfile( reconDir, 'cine_vol' );
-pngfigDir   = fullfile( resultsDir, 'figs', 'png' );
-matfigDir   = fullfile( resultsDir, 'figs', 'fig' );
+pngfigDir   = fullfile( resultsDir, 'figs_swp2cv', 'png' );
+matfigDir   = fullfile( resultsDir, 'figs_swp2cv', 'fig' );
 isVerbose   = false;
 
-nStack      = numel( S );
+mkdir(pngfigDir);
+mkdir(matfigDir);
 
+nStack      = numel( S );
+nStack = 1;
 
 %% Load Data
 
@@ -76,32 +79,295 @@ for iStk = 1:nStack
     P(iStk) = MAT.PARAM;
 
     % Load Sweep Data
-    R(iStk) = load_untouch_nii( S(iStk).rltAbSwpApodFile );
+    R(iStk) = load_untouch_nii( fullfile( dataDir, [ S(iStk).desc '_rlt_swp3d_ab.nii.gz' ] ) );
     
     % Load Masks
-    N = load_untouch_nii( S(iStk).maskHeartSwpApodFile );
-    M{iStk} = single( N.img );
+    N = load_untouch_nii( fullfile( maskDir, [ S(iStk).desc '_mask_heart_swp3d.nii.gz' ] ) );
+    M(iStk).img = single( N.img );
 
+    % Load Cine Vol
+    X(iStk) = load_untouch_nii( fullfile( dataDir, [ 'cine_vol_trans2swp_' S(iStk).desc '.nii.gz' ] ) );
+    X(iStk).img = single( X(iStk).img );
+    
+    % Cine Vol background
+    B(iStk).img = X(iStk).img(:,:,:,1) > 0;
+    
     clear N MAT
     
 end
 
 
-%% Blood-pool Threshold
+%% Auto Crop to cine_vol x/y dimensions
 
 for iStk = 1:nStack
+    for iLoc = 1:size( X(iStk).img,3 )
+        
+        if ~any( any( X(iStk).img(:,:,iLoc,1) ) )
+            isEmptySlice(iLoc) = true;
+            
+            topRow(iLoc)      = nan;
+            bottomRow(iLoc)   = nan;
+            leftColumn(iLoc)  = nan;
+            rightColumn(iLoc) = nan;
+            
+        elseif any( any( X(iStk).img(:,:,iLoc,1) ) )
+            isEmptySlice(iLoc) = false;
+            
+            [ nonZeroRows, nonZeroColumns ] = find( X(iStk).img(:,:,iLoc,1) );
+            
+            topRow(iLoc)      = min( nonZeroRows(:) );
+            bottomRow(iLoc)   = max( nonZeroRows(:) );
+            leftColumn(iLoc)  = min( nonZeroColumns(:) );
+            rightColumn(iLoc) = max( nonZeroColumns(:) );
+            
+        end
+        
+    end
+    
+    cropRegion = [ min(topRow), max(bottomRow), min(leftColumn), max(rightColumn) ];
+    
+    % Apply Crop
+    R(iStk).img = R(iStk).img( cropRegion(1):cropRegion(2), cropRegion(3):cropRegion(4), : );
+    M(iStk).img = M(iStk).img( cropRegion(1):cropRegion(2), cropRegion(3):cropRegion(4), : );
+    X(iStk).img = X(iStk).img( cropRegion(1):cropRegion(2), cropRegion(3):cropRegion(4), :, : );
+    B(iStk).img = B(iStk).img( cropRegion(1):cropRegion(2), cropRegion(3):cropRegion(4), : );
+    
+end
 
-    % Apply Mask
-    RM{iStk} = R(iStk).img .* M{iStk};
+
+%% Pad z for consistency with original k-t blocks
+
+% TODO: might need to re-introduce padding when apodization fully implemented
+
+% I.img = padarray( I.img, [0,0,12], 0, 'both' );
+% M.img = padarray( M.img, [0,0,12], 0, 'both' );
+% X.img = padarray( X.img, [0,0,12,0], 0, 'both' );
+% B.img = padarray( B.img, [0,0,12], 0, 'both' );
+% 
+% thetaFrame = padarray( thetaFrame, 12, NaN, 'both' );
+
+
+%% View thetaFrame Plots
+
+for iStk = 1:nStack
     
-    % Blood-pool Threshold
-    BP{iStk} = ~(RM{iStk} < prctile( nonzeros( RM{iStk}(:) ), 75 ) );
-    
-    % Apply Threshold
-    RBP{iStk} = R(iStk).img .* BP{iStk};
+    nFrame(iStk)  = P(iStk).Sweep.numSwpLoca;
+    nBlocks(iStk) = nFrame(iStk) / ktBlockSize;
+    % ktBlockSize = 32;
+
+    figure('units','normalized','outerposition',[0.1 0.1 0.8 0.8]);
+    hold on;
+
+    plot( cell2mat( S(iStk).thetaFrame ), 'ok', 'MarkerFaceColor','k' );
+
+    xticks( 1:ktBlockSize:nFrame(iStk) );
+    grid; grid minor;
+    axis([1 nFrame(iStk)+10 -0.1 2*pi+0.1]);
+
+    title( [ S(iStk).desc ' - cardsync\_interslice'] );
+    ylabel('Cardiac Phase (theta)');
+    xlabel('Frame Index');
+
+    % Save Fig
+    hFig = gcf;
+    hFig.Name = sprintf( '%s_thetaFrame_cardsync_interslice', S(iStk).desc );
+    save_figs( pngfigDir, hFig, matfigDir )
+
+    if ~isVerbose
+        close( hFig )
+    end
 
 end
 
+
+%% View cine vol at k-t Block location
+if isVerbose    
+    iStk1 = 1;
+    
+    locStart  = 513;
+    locRange  = locStart:locStart+ktBlockSize-1;
+    
+    implay_RR( [R(iStk1).img(:,:,locRange).*B(iStk1).img(:,:,locRange) ...
+        R(iStk1).img(:,:,locRange).*B(iStk1).img(:,:,locRange).*M(iStk1).img(:,:,locRange) ...
+        X(iStk1).img(:,:,locRange,1) ...
+        X(iStk1).img(:,:,locRange,1).*M(iStk1).img(:,:,locRange)] );
+end
+
+
+%% Stack-Cine Vol Cardiac Synchronisation
+
+% Loop over stacks
+for iStk = 1:nStack
+    
+    disp( ['Performing Stack-Cine Vol synchronisation on stack no.: ' num2str(iStk) ] );
+
+    % Loop over k-t blocks
+    thetaFrameModelled = nan( size( cell2mat( S(iStk).thetaFrame ) ) );    
+    ktBlockStartIdx    = 1 : ktBlockSize : nFrame(iStk);
+    thetaFrame         = cell2mat( S(iStk).thetaFrame );
+    tRR                = repelem( cell2mat( S(iStk).tRR ), P(iStk).Sweep.swpWinFullWidth );
+    
+%     for iKT = 1:nBlocks(iStk)
+    for iKT = 3
+        
+        %% Compare k-t block with corresponding cine_vol
+        % ktBlockStart  = 513;
+        ktBlockStart  = ktBlockStartIdx( iKT );
+        ktBlockRange  = ktBlockStart:ktBlockStart+ktBlockSize-1;
+        ktBlockCentre = ktBlockRange( floor(ktBlockSize/2) );
+        
+        ktBlockthetaFrame = thetaFrame( ktBlockRange );
+        ktBlocktRR        = mean( tRR(ktBlockRange) );
+        
+        if isVerbose
+            % cine_vol at centre of k-t block
+            implay_RR( X(iStk).img(:,:,ktBlockCentre,:) );
+        end
+        
+        %% Check for Heart Voxels
+        if ~any( M(iStk).img(:,:,ktBlockRange) , 'all' )    || ...
+           ~any( X(iStk).img(:,:,ktBlockCentre,:) , 'all' ) || ...
+            any( isnan( thetaFrame(ktBlockRange) ) )
+            
+            disp( ['Using original thetaFrame values for k-t block ' num2str(iKT) ' of ' num2str(nBlocks) '...'] );
+            thetaFrameModelled( ktBlockRange ) = ktBlockthetaFrame;
+            
+        else
+            
+            disp( ['Performing cardsync on k-t block ' num2str( iKT ) ' of ' num2str( nBlocks(iStk) ) '...'] );
+            
+            %% Interp cardiac frames to same number as in k-t block
+            nX = size(X(iStk).img,1); nY = size(X(iStk).img,2); nZ = size(X(iStk).img,3);
+            Xinterp.img = zeros( nX, nY, nZ, ktBlockSize );
+            interpPoints = linspace( 1,25,ktBlockSize );
+            
+            for xx = 1:nX
+                for yy = 1:nY
+                    for tt = 1:nZ
+                        sigt = squeeze( X(iStk).img(xx,yy,tt,:) );
+                        if any(sigt)
+                            Xinterp.img(xx,yy,tt,:) = interp1( sigt, interpPoints );
+                        elseif ~any(sigt)
+                            Xinterp.img(xx,yy,tt,:) = 0;
+                        end
+                    end
+                end
+            end
+                      
+            
+            %% Get Mean Signals from Thresholded Blood Pool
+            Rblock.img = R(iStk).img(:,:,ktBlockRange) .* B(iStk).img(:,:,ktBlockRange) .* M(iStk).img(:,:,ktBlockRange);
+            BP.img     = ~( Rblock.img < prctile( nonzeros( Rblock.img(:) ), 75 ) );
+            RBP.img    = Rblock.img .* BP.img;
+            
+%             Xblock.img = X(iStk).img(:,:,ktBlockRange) .* B(iStk).img(:,:,ktBlockRange) .* M(iStk).img(:,:,ktBlockRange);
+%             % BP.img     = ~( Xblock.img < prctile( nonzeros( Xblock.img(:) ), 75 ) );
+%             XBP.img    = Xblock.img .* BP.img;
+            Xblock.img = squeeze(Xinterp.img(:,:,ktBlockCentre,:)) .* M(iStk).img(:,:,ktBlockRange);
+
+            % Video
+            if isVerbose
+                implay_RR( [ Rblock.img, ...
+                            RBP.img, ... 
+                            squeeze(Xinterp.img(:,:,ktBlockCentre,:)) .* M(iStk).img(:,:,ktBlockRange) ...
+                            ] );
+            end
+            
+            % k-t block Signal
+            for iF = 1:numel( ktBlockRange )
+%                 Sig_R(iF) = mean( nonzeros( R.img(:,:,ktBlockRange(iF)).*B.img(:,:,ktBlockRange(iF)).*M.img(:,:,ktBlockRange(iF)) ) );
+                Sig_R(iF) = mean( nonzeros( RBP.img(:,:,iF) ) );
+            end
+            
+            % Cine Vol Signal
+            for iP = 1:size(Xinterp.img,4)
+                Sig_X(iP) = mean( nonzeros( Xinterp.img(:,:,ktBlockCentre,iP).*M(iStk).img(:,:,ktBlockCentre) ) );
+%                 Sig_X(iP) = mean( nonzeros( Xinterp.img(:,:,ktBlockRange(iF),iP).*M(iStk).img(:,:,ktBlockRange(iF)) ) );
+%                 Sig_X(iP) = mean( nonzeros( XBP.img(:,:,iP) ) );
+            end
+            
+            % circshift Sig_X so timepoint 1 = ED
+            [ ~, idxEndDiastoleFrame ] = max( Sig_X );
+            Sig_X = circshift( Sig_X, -idxEndDiastoleFrame+1 );
+            
+            % Calculate vector of cardiac phases for cine_vol
+            thetaFrameCineVol = linspace( 0,2*pi,ktBlockSize ); % Is 0 == 2pi? Should I do ktBlockSize+1?          
+            
+            %%% PERFORM MINIMISATION
+            kt_block_recon_minim;
+            close all;
+            
+            thetaFrameModelled( ktBlockRange ) = ktBlockthetaFramePerturbed;
+            
+            
+            
+        end
+    end
+    
+    % TODO: reformat to same shape/structure as S.thetaFrame
+    S(iStk).thetaFrameModelled = thetaFrameModelled;
+    S(iStk).thetaFrameModelled = num2cell( reshape( S(iStk).thetaFrameModelled, P(iStk).Sweep.swpWinFullWidth, P(iStk).Sweep.numSwpWindows ) , [1,P(iStk).Sweep.numSwpWindows] );
+    
+end
+
+
+%% Plot thetaFrame before/after
+if isVerbose
+    
+    for iStk = 1:nStack
+        figure( 'units', 'normalized', 'outerposition', [0.1 0.1 0.8 0.8] );
+        hold on;
+        
+        subplot(2,1,1);
+        plot( S(iStk).thetaFrame, 'ok', 'MarkerFaceColor','k' );
+        
+        xticks( 1:ktBlockSize:nFrame(iStk) );
+        grid; grid minor;
+        axis([1 nFrame(iStk)+10 -0.1 2*pi+0.1]);
+        
+        title( [ S(iStk).desc ] );
+        ylabel('Cardiac Phase (theta)');
+        xlabel('Frame Index');
+        
+        subplot(2,1,2);
+        plot( S(iStk).thetaFrameModelled, 'ok', 'MarkerFaceColor','k' );
+        
+        xticks( 1:ktBlockSize:nFrame(iStk) );
+        grid; grid minor;
+        axis([1 nFrame(iStk)+10 -0.1 2*pi+0.1]);
+        
+        title( [ S(iStk).desc ' - after cardsync\_cine\_vol' ] );
+        ylabel('Cardiac Phase (theta)');
+        xlabel('Frame Index');
+    end
+    
+end
+
+
+%% Save Results to Text Files
+
+% TODO: finalise which I need
+
+% fid = fopen( fullfile( resultsDir, 'mean_rrinterval.txt' ), 'w' );
+% fprintf( fid, '%.6f ', mean( cell2mat( [ S.tRR ] ) ) );
+% fclose( fid );
+% fid = fopen( fullfile( resultsDir, 'rrintervals.txt' ), 'w' );
+% fprintf( fid, '%.6f ', cell2mat( [ S.tRR ] ) );
+% fclose( fid );
+fid = fopen( fullfile( resultsDir, 'cardphases_cine_vol_cardsync.txt' ), 'w' );
+fprintf( fid, '%.6f ', cell2mat( [S.thetaFrameModelled] ) ); % TODO: check
+fclose( fid );
+
+
+%% Save Results to .mat File
+
+% Tidy S structure
+for iStk = 1:nStack
+    S(iStk).thetaFrame = S(iStk).thetaFrameModelled;
+end
+S = rmfield( S, 'thetaFrameModelled' );
+
+save( fullfile( resultsDir, 'results_cardsync_sweep_cine_vol.mat' ), 'S', '-v7.3' );
 
 
 
--- wip/sweep/kt_block_recon_minim.m
+++ wip/sweep/kt_block_recon_minim.m
@@ -2,22 +2,23 @@
 
 % Then:
 
-%% Circshift - align frames with cine_vol model
-KTB      = zeros( blockSize, 3 );
-KTB(:,1) = 1:blockSize;             % thetaFrame indices
-KTB(:,2) = Sig_I;                   % frame Signal intensities
-KTB(:,3) = ktBlockthetaFrame;       % thetaFrame
+%% Minimisation 1): Circshift
+% - align frames with cine_vol model
+KTB      = zeros( ktBlockSize, 3 );
+KTB(:,1) = 1:ktBlockSize;             % thetaFrame indices
+KTB(:,2) = Sig_R;                     % frame Signal intensities
+KTB(:,3) = ktBlockthetaFrame;         % thetaFrame
 
 % Sort by cardiac phase
 KTB = sortrows( KTB, 3 );
-Sig_I_sorted = KTB(:,2)';
+Sig_R_sorted = KTB(:,2)';
 ssd = [];
 
 % Perform Minimisation
 for iF = 1:numel( ktBlockRange )
     
-    Sig_I_circ = circshift( Sig_I_sorted, iF-1 );
-    Diff       = Sig_I_circ - Sig_X;
+    Sig_R_circ = circshift( Sig_R_sorted, iF-1 );
+    Diff       = Sig_R_circ - Sig_X;
     ssd(iF)    = sum(Diff(:).^2);
     
 end
@@ -25,14 +26,15 @@ end
 [ssd_min, ssd_min_idx] = min( ssd );
 
 % Shift thetaFrame
-thetaShift = 2*pi * ((ssd_min_idx-1)/blockSize); % -1 correct?
+thetaShift = 2*pi * ((ssd_min_idx-1)/ktBlockSize); % -1 correct?
 KTB        = sortrows( KTB, 1 ); % resort by frame order
 KTB(:,3)   = wrapTo2Pi( KTB(:,3) + thetaShift );
 
 ktBlockthetaFrameShifted = KTB(:,3);
 
 
-%% Perturb R-R interval
+%% Minimisation 2): R-R interval Shift
+% - apply constant offsets to difference between thetaFrame values
 
 % Setup interpolated cine_vol signal curve
 ntheta       = 1000;
@@ -50,60 +52,43 @@ KTB(:,1:2,1:numel(Perturbations)) = repmat( KTB(:,1:2), [1,1,numel( Perturbation
 
 % Apply Perturbations to thetaFrame
 for iP = 1:numel( Perturbations )
-    KTB(:,3,iP) = ktBlockthetaFrameShifted(1) : Perturbations(iP) : ktBlockthetaFrameShifted(1)+(Perturbations(iP)*(blockSize-1));
+    KTB(:,3,iP) = ktBlockthetaFrameShifted(1) : Perturbations(iP) : ktBlockthetaFrameShifted(1)+(Perturbations(iP)*(ktBlockSize-1));
 end
 KTB(:,3,:) = wrapTo2Pi( KTB(:,3,:) );
 
-% Plot
-figure;
-for iP = 1:numel( Perturbations )
-    subplot(4,3,iP); hold on;
-    plot( locRange, KTB(:,3,iP), 'ok', 'MarkerFaceColor','k' );
-    grid; grid minor;
-    axis([locRange(1) locRange(end)+1 -0.1 2*pi+0.1]);
-    title(['Perturbation = ' num2str(deltaTheta - Perturbations(iP))]);
+% Plot Perturbed thetaFrame
+if isVerbose
+    figure;
+    for iP = 1:numel( Perturbations )
+        subplot(4,3,iP); hold on;
+        plot( ktBlockRange, KTB(:,3,iP), 'ok', 'MarkerFaceColor','k' );
+        grid; grid minor;
+        axis([ktBlockRange(1) ktBlockRange(end)+1 -0.1 2*pi+0.1]);
+        title(['Perturbation = ' num2str(deltaTheta - Perturbations(iP))]);
+    end
 end
 
-
-% Minimisation
-figure;
+% Perform Minimisation
 ssd_pert = [];
 
 for iP = 1:numel( Perturbations )
-% for iP = 5
-
-    KTBtemp      = sortrows( KTB(:,:,iP), 3 );
-    Sig_I_sorted = KTBtemp(:,2)';
-    thetaI       = KTBtemp(:,3)';
+    % for iP = 5
     
-    Sig_I_sorted = Sig_I_sorted ./ mean( Sig_I_sorted );
+    KTBtemp      = sortrows( KTB(:,:,iP), 3 );
+    Sig_R_sorted = KTBtemp(:,2)';
+    thetaR       = KTBtemp(:,3)';
+    
+    Sig_R_Matrix(iP,:) = Sig_R_sorted ./ mean( Sig_R_sorted );
     
     % Find interpolated indices of thetaI
-    idxThetaInterp(iP,:) = dsearchn( theta', thetaI' );
+    idxThetaInterp(iP,:) = dsearchn( theta', thetaR' );
     
     % cine_vol interpolated signal
-%     Sig_X_Matrix(iP,:) = Sig_X_interp( idxThetaInterp(iP,:) );
     Sig_X_Matrix(iP,:) = Sig_X_interp( idxThetaInterp(iP,:) ) ./ mean( Sig_X_interp( idxThetaInterp(iP,:) ) );
-
     
     % SSD
-    Diff         = Sig_I_sorted - Sig_X_Matrix(iP,:);
-    ssd_pert(iP) = sum(Diff(:).^2);
-   
-    % Residuals
-    subplot(4,3,iP); hold on;
-    plot( thetaI,  Diff, 'ko' );
-%     plot( thetaI, Sig_I_sorted ./ mean(Sig_I_sorted), 'rx' );
-    title(['Perturbation = ' num2str(deltaTheta - Perturbations(iP))]);
-    
-    
-%     % Plot
-%     subplot(4,3,iP); hold on;
-% %     plot( theta,  Sig_X_interp ./ mean(Sig_X_interp), 'b' );
-% %     plot( thetaI, Sig_I_sorted ./ mean(Sig_I_sorted), 'rx' );
-%     plot( thetaI,  Sig_X_Matrix(iP,:), 'b' );
-%     plot( thetaI, Sig_I_sorted, 'rx' );
-%     title(['Perturbation = ' num2str(deltaTheta - Perturbations(iP))]);
+    Diff(iP,:)   = Sig_R_Matrix(iP,:) - Sig_X_Matrix(iP,:);
+    ssd_pert(iP) = sum(Diff(iP,:).^2);
     
 end
 
@@ -111,45 +96,128 @@ end
 
 % Shift thetaFrame
 KTBtemp = sortrows( KTB(:,:,ssd_pert_min_idx), 1 ); % resort by frame order
-% KTBtemp = sortrows( KTB(:,:,5), 1 ); % resort by frame order
 ktBlockthetaFramePerturbed = KTBtemp(:,3);
 
 
-%% Plot SSDs
-figure; hold on; 
-subplot(1,2,1); 
-plot( ssd, 'r' );
-title('Frame to Cine Alignment');
-subplot(1,2,2); 
-plot( Perturbations, ssd_pert, '-bo' );
-title('R-R Perturbations');
+%% Plots
+% if isVerbose
+    
+    %% k-t Block step-by-step
+    iP0 = find( Perturbations./deltaTheta == 1 ); % no perturbation
+    
+    figure('units','normalized','outerposition',[0.2 0.05 0.6 0.9]);
+
+    subplot(2,3,1);
+    hold on;
+    plot( Sig_R, '-or' );
+    xlabel('k-t block frame number');
+    ylabel('Signal [a.u.]');
+    title([' k-t Block Signal (start = ' num2str(ktBlockStart) ')' ]);
+    
+    subplot(2,3,2);
+    plot( Sig_X, '-ob' );
+    xlabel('Cardiac phase [frame index]');
+    ylabel('Signal [a.u.]');
+    title(' cine\_vol Signal ');
+
+    subplot(2,3,3);
+    plot( ktBlockthetaFrame, Sig_R, 'xr' );
+    xlabel('Cardiac phase [radians]');
+    ylabel('Signal [a.u.]');
+    title(' k-t Block - Before Minimisation ');
+
+    subplot(2,3,4);
+    plot( ssd, '-ko' );
+    xlabel('k-t block frame number');
+    ylabel('SSD [a.u.]');
+    title('SSD - Circshift Frames to Cine Vol');
+    
+    subplot(2,3,5);
+    plot( Perturbations, ssd_pert, '-ko' );
+    xlabel('deltaTheta Offset [radians]');
+    ylabel('SSD [a.u.]');
+    title('SSD - RR Interval Perturbations');
+    
+    subplot(2,3,6); hold on;
+    plot( Sig_X_interp ./ mean(Sig_X_interp ),'b');
+    plot( idxThetaInterp(ssd_pert_min_idx,:), Sig_X_Matrix(ssd_pert_min_idx,:), 'bo');
+    plot( idxThetaInterp(ssd_pert_min_idx,:), Sig_R_Matrix(ssd_pert_min_idx,:), 'rx');
+    xlabel('Cardiac phase [radians]');
+    ylabel('Signal [a.u.]');
+    title(' k-t Block - After Minimisation ');
+    
+    % Save Fig
+    hFig = gcf;
+    hFig.Name = sprintf( '%s_ktblock%s_minim_overview', S(iStk).desc, num2str(ktBlockStart) );
+    save_figs( pngfigDir, hFig, matfigDir )
+    
+    
+    %% Signals after Perturbations
+    figure('units','normalized','outerposition',[0.2 0.2 0.4 0.7]);
+    for iP = 1:numel( Perturbations )
+        subplot(4,3,iP); hold on;
+        plot( thetaR, Sig_X_Matrix(iP,:), 'b' );
+        plot( thetaR, Sig_R_Matrix(iP,:), 'rx' );
+        title(['Perturbation = ' num2str(deltaTheta - Perturbations(iP))]);
+    end
+    
+    % Save Fig
+    hFig = gcf;
+    hFig.Name = sprintf( '%s_ktblock%s_perturbation_signals', S(iStk).desc, num2str(ktBlockStart) );
+    save_figs( pngfigDir, hFig, matfigDir )
+    
+    
+    %% Residuals after Perturbations
+    figure('units','normalized','outerposition',[0.2 0.2 0.4 0.7]);
+    for iP = 1:numel( Perturbations )
+        subplot(4,3,iP); hold on;
+        plot( thetaR,  Diff(iP,:), 'ko' );
+        title(['Perturbation = ' num2str(deltaTheta - Perturbations(iP))]);
+        axis([0 2*pi min(Diff(:)) max(Diff(:))])
+    end
+    
+    % Save Fig
+    hFig = gcf;
+    hFig.Name = sprintf( '%s_ktblock%s_perturbation_residuals', S(iStk).desc, num2str(ktBlockStart) );
+    save_figs( pngfigDir, hFig, matfigDir )
 
 
-%% k-t Block thetaFrame Plots
+    
+    %% k-t Block thetaFrame Before/After
+    figure('units','normalized','outerposition',[0.2 0.2 0.4 0.4]);
+    
+    subplot(1,3,1); hold on;
+    plot( ktBlockRange, ktBlockthetaFrame, 'ok', 'MarkerFaceColor','k' );
+    grid; grid minor;
+    axis([ktBlockRange(1) ktBlockRange(end)+1 -0.1 2*pi+0.1]);
+    title( 'Original Theta' );
+    ylabel('Cardiac Phase (theta)');
+    xlabel('Frame Index');
+    
+    subplot(1,3,2); hold on;
+    plot( ktBlockRange, ktBlockthetaFrameShifted, 'ok', 'MarkerFaceColor','k' );
+    grid; grid minor;
+    axis([ktBlockRange(1) ktBlockRange(end)+1 -0.1 2*pi+0.1]);
+    title( 'Shifted Theta' );
+    ylabel('Cardiac Phase (theta)');
+    xlabel('Frame Index');
+    
+    subplot(1,3,3); hold on;
+    plot( ktBlockRange, ktBlockthetaFrameShifted, 'ok', 'MarkerSize', 9);
+    plot( ktBlockRange, ktBlockthetaFramePerturbed, 'ok', 'MarkerFaceColor','k' );
+    grid; grid minor;
+    axis([ktBlockRange(1) ktBlockRange(end)+1 -0.1 2*pi+0.1]);
+    title( 'Shifted Theta + Perturbations' );
+    ylabel('Cardiac Phase (theta)');
+    xlabel('Frame Index');
+    
+    % Save Fig
+    hFig = gcf;
+    hFig.Name = sprintf( '%s_ktblock%s_thetaframe', S(iStk).desc, num2str(ktBlockStart) );
+    save_figs( pngfigDir, hFig, matfigDir )
+    
+    if ~isVerbose
+        close all;
+    end
 
-figure('units','normalized','outerposition',[0.2 0.2 0.4 0.4]); 
-
-subplot(1,3,1); hold on;
-plot( locRange, ktBlockthetaFrame, 'ok', 'MarkerFaceColor','k' );
-grid; grid minor;
-axis([locRange(1) locRange(end)+1 -0.1 2*pi+0.1]);
-title( 'Original Theta' );
-ylabel('Cardiac Phase (theta)');
-xlabel('Frame Index');
-
-subplot(1,3,2); hold on;
-plot( locRange, ktBlockthetaFrameShifted, 'ok', 'MarkerFaceColor','k' );
-grid; grid minor;
-axis([locRange(1) locRange(end)+1 -0.1 2*pi+0.1]);
-title( 'Shifted Theta' );
-ylabel('Cardiac Phase (theta)');
-xlabel('Frame Index');
-
-subplot(1,3,3); hold on;
-plot( locRange, ktBlockthetaFrameShifted, 'ok', 'MarkerSize', 9);
-plot( locRange, ktBlockthetaFramePerturbed, 'ok', 'MarkerFaceColor','k' );
-grid; grid minor;
-axis([locRange(1) locRange(end)+1 -0.1 2*pi+0.1]);
-title( 'Shifted Theta + Perturbations' );
-ylabel('Cardiac Phase (theta)');
-xlabel('Frame Index');
+% end
